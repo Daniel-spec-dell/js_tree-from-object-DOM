2de5734bc952ad2f5599af4b6922258b3f6b3875
Daniel-spec-dell <fvgdanil@gmail.com>
2025-06-22 12:21:06 +0300

    add task solutio ver 1.01

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -35,7 +35,7 @@ function createTree(element, data) {
 
     li.textContent = key;
 
-    if (typeof data[key] === 'object' && Object.keys(data).length > 0) {
+    if (typeof data[key] === 'object' && Object.keys(data[key]).length > 0) {
       createTree(li, data[key]);
     }
 
